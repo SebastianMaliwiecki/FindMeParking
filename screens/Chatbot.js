ddd39abec0cdb07299502f901c13f012f55fd614
Sebastian Maliwiecki <60139438+SebastianMaliwiecki@users.noreply.github.com>
2022-04-03 16:38:11 +0100

    Update to car info page

--- screens/Chatbot.js
+++ screens/Chatbot.js
@@ -1,7 +1,8 @@
 import { getStorage, ref, getDownloadURL } from "firebase/storage";
 import React, {useState, useEffect} from 'react'
-import { StyleSheet, Text, View, Image } from 'react-native'
+import { StyleSheet, Text, View, Image, SafeAreaView } from 'react-native'
 import { useUserContext } from '../context/UserContext'
+import { AntDesign } from '@expo/vector-icons'
 
 const Chatbot = () => {
 
@@ -25,14 +26,22 @@ const Chatbot = () => {
 
     useEffect( async () => {
         const storage = getStorage();
-        const pathReference = ref(storage, '/Ulez_sign/ULEZ_sign_compliant.png');
+        const ulezSigns = ['ULEZ_sign_compliant.png', 'ULEZ_sign_NonCompliant.png']
+        const pathReference = ref(storage, `/Ulez_sign/${ulezData.compliance=="Compliant" ? ulezSigns[0] : ulezSigns[1]}`);
         await getDownloadURL(pathReference).then(x => {
             setUlezImage(x)
         })
     }, [ulezData])
 
     return (
-        <View style={styles.container}>
+        <SafeAreaView style={styles.container}>
+            <View style={styles.header}>
+                <Text style={styles.headerText}>
+                    <Text style={{color: 'rgb(56,166,255)'}}>
+                        Car Info
+                    </Text>
+                </Text>
+            </View>
             <View style={{flexDirection:'row'}}>
                 <View style={styles.blueBox}/>
                 <View style={styles.numerplate}>
@@ -45,14 +54,33 @@ const Chatbot = () => {
                 ulezData ?
                 (
                     <View style={styles.detailsContainer}>
-                        <Text style={styles.carDetails}>{ulezData.colour} {ulezData.make} {ulezData.model}</Text>
-                        <Image
-                            source={{uri:ulezImage}}
-                            style={{
-                                width: 300,
-                                height:400
-                            }}
-                        />
+                        <Text style={styles.carDetails}>{ulezData.colour} <Text style={{fontWeight:'bold'}}>{ulezData.make} {ulezData.model}</Text></Text>
+                            {/* <View style={{alignItems: 'center'}}>
+                                <Image
+                                    source={{uri:ulezImage}}
+                                    style={{
+                                        width: 200,
+                                        height: 330,
+                                        resizeMode: 'contain',
+                                        borderColor: 'white',
+                                        //borderWidth: 1,
+                                        alignItems: 'center'
+                                    }}
+                                />
+                            </View> */}
+                            <View style={{flexDirection:'row', padding: 5, backgroundColor: 'rgb(13,17,23)', margin: 5, borderRadius: 15}}>
+                                <View style={{backgroundColor: 'rgb(94,151,50)', justifyContent: 'center', alignItems: 'center', borderRadius:7, width: 50, height: 50 }}>
+                                    <AntDesign name="checkcircle" size={32} color="white" />
+                                </View>
+                                <Text style={{color: 'white', fontSize: 17,  fontWeight: 'bold', flex: 1, flexWrap: 'wrap', paddingLeft: 7}}>
+                                    {
+                                    ulezData.compliance=="Compliant" ?
+                                            'Your vehicle meets the emission standards to enter Ultra Low Emission Zone or Low Emission Zone charge, no charge will apply entering those zones.'
+                                        :
+                                            'Your vehicle does not meet the emission standards to enter Ultra Low Emission Zone or Low Emission Zone charge, charges will apply when entering those zones.'
+                                    }
+                                </Text>
+                            </View>
                     </View>
                 )
                 :
@@ -62,7 +90,7 @@ const Chatbot = () => {
                     </Text>
                 )
             }
-        </View>
+        </SafeAreaView>
     )
 }
 
@@ -71,8 +99,7 @@ export default Chatbot
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
+        //justifyContent: 'center',
         backgroundColor: 'rgb(7,10,14)'
     },
     blueBox: {
@@ -97,12 +124,23 @@ const styles = StyleSheet.create({
         color: 'white',
     },
     detailsContainer: {
-        alignItems: 'center'
+        //alignItems: 'center'
     },
     carDetails: {
         fontSize: 20,
         color: 'white',
         padding: 7,
         textAlign:'center'
-    }
+    },
+    header: {
+        width: 'auto',
+        height: 'auto',
+        justifyContent: 'center',
+    },
+    headerText: {
+        fontSize: 30,
+        padding: 10,
+        fontWeight: "700",
+        marginLeft: 20,
+    }, 
 })
